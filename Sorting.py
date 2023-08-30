ee54b76f8e82c7895579b964f197cc2c1fa384e6
Sathvik Shetty <119489811+SathvikShetty1@users.noreply.github.com>
2023-08-30 19:31:41 +0530

    Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -4,11 +4,9 @@ import matplotlib.pyplot as plt
 
 def mergeSort(array):
     if len(array) > 1:
-
-        r =len(array)//2
+        r = len(array) // 2
         L = array[:r]
         M = array[r:]
-
 
         mergeSort(L)
         mergeSort(M)
@@ -24,7 +22,6 @@ def mergeSort(array):
                 j += 1
             k += 1
 
-
         while i < len(L):
             array[k] = L[i]
             i += 1
@@ -32,25 +29,20 @@ def mergeSort(array):
 
         while j < len(M):
             array[k] = M[j]
-            j+= 1
+            j += 1
             k += 1
 
-
 def partition(array, low, high):
-
     pivot = array[high]
     i = low - 1
 
     for j in range(low, high):
         if array[j] <= pivot:
+            i = i + 1
+            array[i], array[j] = array[j], array[i]
 
-            i = i+1
-
-            (array[i], array[j]) = (array[j], array[i])
-
-    (array[i+1], array[high]) = (array[high], array[i+1])
-    return i+1
-
+    array[i + 1], array[high] = array[high], array[i + 1]
+    return i + 1
 
 def quickSort(array, low, high):
     if low < high:
@@ -58,78 +50,72 @@ def quickSort(array, low, high):
         quickSort(array, low, pi - 1)
         quickSort(array, pi + 1, high)
 
-
 def selectionSort(array, size):
     for step in range(size):
         min_idx = step
-
         for i in range(step + 1, size):
-
             if array[i] < array[min_idx]:
                 min_idx = i
-        (array[step], array[min_idx]) = (array[min_idx], array[step])
-
+        array[step], array[min_idx] = array[min_idx], array[step]
 
 def read_Input():
-    a=[]
-    n=int(input("Enter the number of TV Channels:"))
-    print("Enter the no.of viewers")
-    for i in range(0,n):
-        l=int(input())
+    a = []
+    n = int(input("Enter the number of TV Channels: "))
+    print("Enter the number of viewers")
+    for i in range(0, n):
+        l = int(input())
         a.append(l)
-    return a;
+    return a
 
 elements = list()
 times = list()
-global labeldata
+
 print("1. Merge Sort 2. Quick Sort 3. Selection Sort")
-ch = int(input("Enter the Choice :"))
-if (ch==1):
-    array=read_Input()
-    mergeSort(array)
-    labeldata="MergeSort"
-    print("Sorted Array is:")
-    print(array)
-elif(ch==2):
-    array=read_Input()
-    size = len(array)
-    labeldata="QuickSort"
-    quickSort(array, 0, size - 1)
-    print('Sorted Array is:')
-    print(array)
-if (ch==3):
-    array=read_Input()
-    size = len(array)
-    labeldata="SelectionSort"
-    selectionSort(array, size)
-    print('Sorted Array is:')
-    print(array)
-print("***************************Running Time Analysis************************")
-for i in range (1,11):
+ch = int(input("Enter the Choice: "))
 
-    array = randint(0, 1000*i, 1000*i)
-    start=time.time()
+if ch in [1, 2, 3]:
+    array = read_Input()
 
-    if ch==1:
+    if ch == 1:
+        labeldata = "MergeSort"
         mergeSort(array)
-    elif ch==2:
-        size=len(array)
-        quickSort(array, 0, size-1)
-    else:
-        size=len(array)
+    elif ch == 2:
+        labeldata = "QuickSort"
+        size = len(array)
+        quickSort(array, 0, size - 1)
+    elif ch == 3:
+        labeldata = "SelectionSort"
+        size = len(array)
         selectionSort(array, size)
 
-    end=time.time()
+    print("Sorted Array is:")
+    print(array)
 
-    print("Sorted List is:")
-    print(len(array), "Elements Sorted by", labeldata,end-start)
-    elements.append(len(array))
-    times.append(end-start)
+    print("***************************Running Time Analysis************************")
+    for i in range(1, 11):
+        array = randint(0, 1000 * i, 1000 * i)
+        start = time.time()
 
-plt.xlabel('List Length')
-plt.ylabel('Time Complexity')
-labeldata="sort"
-plt.plot(elements, times, label=labeldata)
-plt.grid()
-plt.legend()
-plt.show(
+        if ch == 1:
+            mergeSort(array)
+        elif ch == 2:
+            size = len(array)
+            quickSort(array, 0, size - 1)
+        elif ch == 3:
+            size = len(array)
+            selectionSort(array, size)
+
+        end = time.time()
+
+        print(len(array), "Elements Sorted by", labeldata, "in", end - start, "seconds")
+        elements.append(len(array))
+        times.append(end - start)
+
+    plt.xlabel('List Length')
+    plt.ylabel('Time Complexity')
+    plt.plot(elements, times, label=labeldata)
+    plt.grid()
+    plt.legend()
+    plt.show()
+else:
+    print("Invalid choice. Please select 1, 2, or 3 for sorting algorithms.")
